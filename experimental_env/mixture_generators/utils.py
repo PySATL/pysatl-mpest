aac0c6da097926ecc51fbe5193675fd5a7bcca3f
iraedeus <dtotjmyanin@mail.ru>
2025-07-17 18:32:40 +0300

    fix: ci

--- experimental_env/mixture_generators/utils.py
+++ experimental_env/mixture_generators/utils.py
@@ -3,8 +3,7 @@
 from random import uniform
 
 from mpest import Distribution
-from mpest.models import AModel, Beta, Cauchy, ExponentialModel, GaussianModel, Pareto
-from mpest.models.uniform import Uniform
+from mpest.models import AModel, ExponentialModel, GaussianModel
 
 
 def generate_standart_params(models: list[type[AModel]]) -> list[Distribution]:
@@ -15,12 +14,8 @@ def generate_standart_params(models: list[type[AModel]]) -> list[Distribution]:
     for m in models:
         if m == ExponentialModel:
             params = [1.0]
-        elif m in (GaussianModel, Uniform, Cauchy):
+        elif m == GaussianModel:
             params = [0.0, 1.0]
-        elif m == Beta:
-            params = [1.0, 1.0]
-        elif m == Pareto:
-            params = [1.0, 2.0]
         else:  # Weibull
             params = [1.0, 1.0]
 
@@ -39,14 +34,6 @@ def generate_uniform_params(models: list[type[AModel]]) -> list[Distribution]:
             params = [uniform(0.1, 5.0)]
         elif m == GaussianModel:
             params = [uniform(-5.0, 5.0), uniform(0.1, 5.0)]
-        elif m == Uniform:
-            params = list(sorted([uniform(-5.0, 5.0), uniform(-5.0, 5.0)]))
-        elif m == Cauchy:
-            params = [uniform(-5.0, 5.0), uniform(0.1, 5.0)]
-        elif m == Beta:
-            params = [uniform(0.1, 5.0), uniform(0.1, 5.0)]
-        elif m == Pareto:
-            params = [uniform(0.1, 5.0), uniform(1.0, 5.0)]
         else:  # Weibull
             params = [uniform(0.1, 5.0), uniform(0.1, 5.0)]
 
